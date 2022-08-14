0276aee2d4988708898f5ccd69a8ebb2f0126c2e
bregarulai <bregarulai@gmail.com>
2022-08-14 13:51:13 -0400

    crated app main structure

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,7 +1,22 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import { Menu, Navbar } from './components';
+
+const Container = styled.div``;
+
+const Main = styled.div``;
+const Wrapper = styled.div``;
 
 function App() {
-  return <div>app</div>;
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>Video Cards</Wrapper>
+      </Main>
+    </Container>
+  );
 }
 
 export default App;
