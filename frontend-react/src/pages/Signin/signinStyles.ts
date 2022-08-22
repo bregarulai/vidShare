24c0d0700a518923ff3a4f00798dcf615b03f79f
bregarulai <bregarulai@gmail.com>
2022-08-22 17:49:21 -0400

    added styles to sigin page

--- frontend-react/src/pages/Signin/signinStyles.ts
+++ frontend-react/src/pages/Signin/signinStyles.ts
@@ -1,5 +1,4 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -7,35 +6,58 @@ export const Container = styled.div`
   justify-content: center;
   flex-direction: column;
   height: calc(100vh - 56px);
-`
+`;
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: ${({theme}) => theme.bgLighter};
-  border: 1px solid ${({theme}) => theme.soft};
+  background-color: ${({ theme }) => theme.bgLighter};
+  border: 1px solid ${({ theme }) => theme.soft};
   padding: 20px 50px;
   gap: 10px;
-`
+`;
 
 export const Title = styled.h1`
+  font-size: 24px;
+`;
 
-`
 export const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+`;
 
-`
 export const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  outline: none;
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+  width: 100%;
+`;
 
-`
 export const Button = styled.button`
+  border: none;
+  outline: none;
+  padding: 10px 20px;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-`
 export const More = styled.div`
+  display: flex;
+  font-size: 12px;
+  margin-top: 10px;
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-`
 export const Links = styled.div`
+  margin-left: 50px;
+`;
 
-`
 export const Link = styled.span`
-
-`
+  margin-left: 30px;
+  cursor: pointer;
+`;
